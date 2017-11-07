cd1610a179d82ab32906742c0aac615b8b8cd93e
Kota Nonaka <kota.kaicho@gmail.com>
2017-11-07 15:23:54 +0900

    Fix order posts

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,6 @@
 class WelcomeController < ApplicationController
   def index
     per = 5 # 1ページに表示するPost数
-    @post = Post.page(params[:page]).per(per)
+    @post = Post.page(params[:page]).order("created_at desc").per(per)
   end
 end
